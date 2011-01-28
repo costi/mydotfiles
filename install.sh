61276f5f631f9621fc974d45dc6d340790d79ff7
Costi <costin@gmx.net>
2011-01-28 10:03:16 -0600

    added comment on how to install vim-ruby for fuzzy file finder

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# install these manually:
+# exuberant-ctags and vim-ruby (for fuzzy file finder)
+# sudo apt-get install -y exuberant-ctags
+# sudo apt-get install -y vim-ruby
+
 cp -va * ~
 tar -cvf - .* --exclude=\. --exclude=\.\. --exclude=\.git | tar -xf - -C ~/
 
