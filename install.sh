6a218754243b795383229fb2a6156f5f38901092
Costi <cgavrilescu@enovafinancial.com>
2011-11-30 09:24:14 -0600

    Adds .gitconfig

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 ME=`readlink -f $0`
 SETUP_DIR=$(dirname $ME)
 
-files=( .ackrc .bashrc .irbrc .bash_aliases .screenrc .vimrc .vim .profile bin/make-tags-file)
+files=( .ackrc .bashrc .irbrc .bash_aliases .screenrc .vimrc .vim .profile bin/make-tags-file .gitconfig)
 for file in "${files[@]}"
 do
   rm -vr ~/${file}
