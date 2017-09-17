2925638230cf05c32f8fcb61fd6b614c0aa8fe3e
Costi <comisarulmoldovan@gmail.com>
2017-09-16 22:36:44 -0500

    Adds ack

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 sudo apt-get install -y exuberant-ctags
 #sudo apt-get install -y vim-ruby # vim-ruby is provided by either vim-gtk or vim-gnome
 sudo apt-get install -y vim-gtk
+sudo apt-get install -y ack
 
 git submodule init
 git submodule update # to fetch liquidprompt
